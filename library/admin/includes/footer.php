917b1ff2ff5ed83b481ce853f0025e87eb630161
Ankur Khandelwal <khandelwalankur176@gmail.com>
2021-03-06 13:51:17 +0530

    Changed the footer content

--- library/admin/includes/footer.php
+++ library/admin/includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; 2017 Online Library Management System |<a href="https://phpgurukul.com/" target="_blank"  > Designed by : PHPGURUKUL</a> 
+                   &copy; 2021 IIIT Bhubaneswar Library Information System | Developed by Ankur, Rajesh and Saunak 
                 </div>
 
             </div>
