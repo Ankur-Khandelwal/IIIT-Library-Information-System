10127d870014a0724bf7f0c219f82ae47e381507
Ankur Khandelwal <khandelwalankur176@gmail.com>
2021-03-09 12:12:02 +0530

    Some Utility changes

--- library/admin/manage-books.php
+++ library/admin/manage-books.php
@@ -14,7 +14,7 @@ $sql = "delete from tblbooks  WHERE id=:id";
 $query = $dbh->prepare($sql);
 $query -> bindParam(':id',$id, PDO::PARAM_STR);
 $query -> execute();
-$_SESSION['delmsg']="Category deleted scuccessfully ";
+$_SESSION['delmsg']="Category deleted successfully ";
 header('location:manage-books.php');
 
 }
